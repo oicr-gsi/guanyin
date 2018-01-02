f32db0e0e872b7b02936dbaa34d59e4e4156777f
Xuemei Luo <xuemei.luo@oicr.on.ca>
2018-01-02 16:40:04 -0500

    updated the API url

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var db = require('../queries');
 
 router.get('/reportdb/reports', db.getAllReports);
 router.get('/reportdb/report/:id', db.getSingleReport);
+router.get('/reportdb/report', db.getAllreports_by_name);
 router.post('/reportdb/report', db.createReport);
 router.put('/reportdb/report/:id', db.updateReport);
 router.get('/reportdb/reportrecords', db.getAllReportrecords);
@@ -13,6 +14,6 @@ router.get('/reportdb/reportrecord', db.getAllreportrecords_by_notification_done
 router.post('/reportdb/reportrecord', db.createReportrecord);
 router.put('/reportdb/reportrecord/:id', db.updateReportrecord);
 router.put('/reportdb/reportrecord/:id/notification_done', db.updateReportrecord_notification_done);
-router.post('/reportdb/files_in', db.findReportrecord_files_in);
-router.post('/reportdb/parameters', db.findReportrecord_parameters);
+router.post('/reportdb/reportrecord/search', db.findReportrecord_files_in);
+router.post('/reportdb/reportrecord/parameters', db.findReportrecord_parameters);
 module.exports = router;
