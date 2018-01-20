dec811b9b8bbaa9586e92885a34b246395a6fd80
Xuemei Luo <xuemei.luo@oicr.on.ca>
2018-01-20 00:39:35 -0500

    added test

--- queries.js
+++ queries.js
@@ -47,31 +47,13 @@ function getAllreports_by_name (req, res, next) {
 
 
 function createReport(req, res, next) {
-  db.none('insert into report(name, version, category, permitted_parameters)' +
-      'values(${name}, ${version}, ${category}, ${permitted_parameters})',
+  db.one('insert into report(name, version, category, permitted_parameters)' +
+      'values(${name}, ${version}, ${category}, ${permitted_parameters})' +
+      'returning report_id',
     req.body)
-    .then(function () {
+    .then(function (data) {
       res.status(200)
-        .json({
-          status: 'success',
-          message: 'Inserted one report'
-        });
-    })
-    .catch(function (err) {
-      return next(err);
-    });
-}
-
-function updateReport(req, res, next) {
-  db.none('update report set name=$1, version=$2, category=$3, permitted_parameters=$4 where report_id=$5',
-    [req.body.name, req.body.version, req.body.category,
-      req.body.permitted_parameters, parseInt(req.params.id)])
-    .then(function () {
-      res.status(200)
-        .json({
-          status: 'success',
-          message: 'Updated report'
-        });
+        .json(data);
     })
     .catch(function (err) {
       return next(err);
@@ -116,32 +98,13 @@ function getSingleReportrecord(req, res, next) {
 
 function createReportrecord(req, res, next) {
   req.body.report_id = parseInt(req.body.report_id);
-  db.none('insert into report_record(report_id, files_in, report_path, notification_targets, notification_message, parameters)' +
-      'values(${report_id}, ${files_in}, ${report_path}, ${notification_targets}, ${notification_message}, ${parameters})',
+  db.one('insert into report_record(report_id, date_generated, freshest_input_date, files_in, report_path, notification_targets, notification_message, parameters)' +
+      'values(${report_id}, ${date_generated}, ${freshest_input_date}, ${files_in}, ${report_path}, ${notification_targets}, ${notification_message}, ${parameters})' +
+      'returning report_record_id',
     req.body)
-    .then(function () {
+    .then(function (data) {
       res.status(200)
-        .json({
-          status: 'success',
-          message: 'Inserted one report record'
-        });
-    })
-    .catch(function (err) {
-      return next(err);
-    });
-}
-
-
-function updateReportrecord(req, res, next) {
-  db.none('update report_record set report_id=$1, files_in=$2, report_path=$3, notification_targets=$4, notification_message=$5,notification_done=$6, parameters=$7 where report_record_id=$8',
-    [parseInt(req.body.report_id), req.body.files_in, req.body.report_path,
-      req.body.notification_targets, req.body.notification_message, req.body.notification_done, req.body.parameters, parseInt(req.params.id)])
-    .then(function () {
-      res.status(200)
-        .json({
-          status: 'success',
-          message: 'Updated report record'
-        });
+        .json(data);
     })
     .catch(function (err) {
       return next(err);
@@ -150,7 +113,7 @@ function updateReportrecord(req, res, next) {
 
 function updateReportrecord_notification_done(req, res, next) {
   db.none('update report_record set notification_done=$1 where report_record_id=$2',
-    [req.body.notification_done, parseInt(req.params.id)])
+    [req.query.notification_done, parseInt(req.query.report_record_id)])
     .then(function () {
       res.status(200)
         .json({
@@ -165,7 +128,7 @@ function updateReportrecord_notification_done(req, res, next) {
 
 function findReportrecord_files_in (req, res, next) {
   
-  db.one('select rr.* from report_record rr, report r  where r.report_id=rr.report_id and r.name = $1 and r.version = $2 and rr.files_in= $3',
+  db.any('select rr.* from report_record rr, report r  where r.report_id=rr.report_id and r.name = $1 and r.version = $2 and rr.files_in= $3',
    [req.query.name, req.query.version, req.body.files_in])
     .then(function (data) {
       res.status(200)
@@ -178,7 +141,7 @@ function findReportrecord_files_in (req, res, next) {
 
 function findReportrecord_parameters (req, res, next) {
 
-  db.one('select rr.* from report_record rr, report r  where r.report_id=rr.report_id and r.name = $1 and r.version = $2 and rr.parameters= $3',
+  db.any('select rr.* from report_record rr, report r  where r.report_id=rr.report_id and r.name = $1 and r.version = $2 and rr.parameters= $3',
    [req.query.name, req.query.version, req.body.parameters])
     .then(function (data) {
       res.status(200)
@@ -195,11 +158,9 @@ module.exports = {
   getSingleReport: getSingleReport,
   getAllreports_by_name:getAllreports_by_name,
   createReport: createReport,
-  updateReport: updateReport,
   getAllReportrecords: getAllReportrecords,
   getSingleReportrecord: getSingleReportrecord,
   createReportrecord: createReportrecord,
-  updateReportrecord: updateReportrecord,
   updateReportrecord_notification_done: updateReportrecord_notification_done,
   getAllreportrecords_by_notification_done: getAllreportrecords_by_notification_done,
   findReportrecord_files_in: findReportrecord_files_in,
---
+++ test/test-server.js
@@ -0,0 +1,285 @@
+process.env.NODE_ENV = 'test';
+
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../app');
+var should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('report', function() {
+  it('should list ALL reports on /reportdb/reports GET', function(done) {
+    chai.request(server)
+      .get('/reportdb/reports')
+      .end(function(err, res){
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body[0].should.have.property('report_id');
+        res.body[0].should.have.property('name');
+        res.body[0].should.have.property('version');
+        res.body[0].should.have.property('category');
+        res.body[0].should.have.property('permitted_parameters');   
+        done();
+      });
+  });
+
+  it('should list a single report on /reportdb/report/:id GET', function(done) {
+    chai.request(server)
+      .get('/reportdb/report/1')
+      .end(function(err, res){
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.have.property('report_id');
+        res.body.should.have.property('name');
+        res.body.should.have.property('version');
+        res.body.should.have.property('category');
+        res.body.should.have.property('permitted_parameters');   
+        res.body.report_id.should.equal(1);   
+        done();
+      });
+  });
+
+  it('should list reports on /reportdb/report?name={name} GET', function(done) {
+    chai.request(server)
+      .get('/reportdb/report?name=jsonReport')
+      .end(function(err, res){
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body[0].should.have.property('report_id');
+        res.body[0].should.have.property('name');
+        res.body[0].should.have.property('version');
+        res.body[0].should.have.property('category');
+        res.body[0].should.have.property('permitted_parameters');   
+        res.body[0].name.should.equal('jsonReport');   
+        done();
+      });
+  });
+
+  it('should add a SINGLE report on /reportdb/report POST', function(done) {
+    chai.request(server)
+      .post('/reportdb/report')
+      .send({
+        "name": "jsonReport",
+        "version": "1.0",
+        "category": "report",
+        "permitted_parameters": {
+            "type":"object", 
+            "required":"false",
+            "properties": {"Instrument":{"type":"string", "required":"false"},
+                           "runName":{"type":"string", "required":"false"}}
+         }
+        })
+      .end(function(err, res){
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.have.property('report_id');
+        done();
+      });
+  });
+});
+
+describe('report_record', function() {
+  it('should list ALL report records on /reportdb/records GET', function(done) {
+    chai.request(server)
+      .get('/reportdb/records')
+      .end(function(err, res){
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body[0].should.have.property('report_record_id');
+        res.body[0].should.have.property('report_id');
+      res.body[0].should.have.property('date_generated');
+      res.body[0].should.have.property('freshest_input_date');
+      res.body[0].should.have.property('files_in');
+      res.body[0].should.have.property('report_path');
+      res.body[0].should.have.property('notification_targets');
+      res.body[0].should.have.property('notification_message');
+      res.body[0].should.have.property('notification_done');
+      res.body[0].should.have.property('parameters');
+      done();
+    });
+  });
+
+  it('should list a single report record on /reportdb/record/:id GET', function(done) {
+    chai.request(server)
+      .get('/reportdb/record/1')
+      .end(function(err, res){
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.have.property('report_record_id');
+        res.body.should.have.property('report_id');
+        res.body.should.have.property('date_generated');
+        res.body.should.have.property('freshest_input_date');
+        res.body.should.have.property('files_in');
+        res.body.should.have.property('report_path');
+        res.body.should.have.property('notification_targets');
+        res.body.should.have.property('notification_message');
+        res.body.should.have.property('notification_done');
+        res.body.should.have.property('parameters');
+        res.body.report_record_id.should.equal(1);   
+        done();
+      });
+  });
+
+  it('should list reports on /reportdb/report?notification_done={notification_done} GET', function(done) {
+    chai.request(server)
+      .get('/reportdb/record?notification_done=false')
+      .end(function(err, res){
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body[0].should.have.property('report_record_id');
+        res.body[0].should.have.property('report_id');
+        res.body[0].should.have.property('date_generated');
+        res.body[0].should.have.property('freshest_input_date');
+        res.body[0].should.have.property('files_in');
+        res.body[0].should.have.property('report_path');
+        res.body[0].should.have.property('notification_targets');
+        res.body[0].should.have.property('notification_message');
+        res.body[0].should.have.property('notification_done');
+        res.body[0].should.have.property('parameters'); 
+        res.body[0].notification_done.should.equal(false);   
+        done();
+      });
+  });
+
+
+  it('should add a SINGLE report record on /reportdb/record POST', function(done) {
+    chai.request(server)
+      .post('/reportdb/record')
+      .send({
+    	"report_id": 4,
+        "date_generated": "2018-01-19T14:43:12.518Z",
+        "freshest_input_date": "2018-01-19T14:43:12.518Z",
+        "files_in": {
+          "file_path":["p1", "p2", "P3", "P4"]
+        },
+       "report_path": "https://www.hpc.oicr.on.ca/archive/web/runReports/180109_D00331_0293_BCC5BJANXX/180109_D00331_0293_BCC5BJANXX_report.html",
+       "notification_targets": {
+          "email": "seqprodbio@lists.oicr.on.ca, GenomeTechnologies@oicr.on.ca"
+        },
+       "notification_message": "Here is the report: https://www.hpc.oicr.on.ca/archive/web/runReports/180109_D00331_0293_BCC5BJANXX/180109_D00331_0293_BCC5BJANXX_report.html",
+       "notification_done": false,
+       "parameters": {
+          "runName":"180109_D00331_0293_BCC5BJANXX", 
+          "instrument":"HiSeq"
+        }
+      })
+      .end(function(err, res){
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.have.property('report_record_id');
+        done();
+      });
+  });
+
+  it('should update a SINGLE report record on /reportdb/record_notification?report_record_id={report_record_id}&notification_done={notification_done} PUT', function(done) {
+    chai.request(server)
+      .put('/reportdb/record_notification?report_record_id=1&notification_done=true')
+      .end(function(err, res){
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.have.property('status');
+        res.body.should.have.property('message');
+        res.body.status.should.equal('success');
+        res.body.message.should.equal('Updated report record'); 
+        done();
+      });
+  });
+
+  it('should get report records by given name, version and files_in', function(done) {
+    chai.request(server)
+    .post('/reportdb/record_files?name=jsonReport&version=1.1')
+    .send ({
+       "files_in": {
+          "file_path":["p1", "p2"]
+        }
+     })
+      .end(function(err, res){
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body[0].should.have.property('report_record_id');
+        res.body[0].should.have.property('report_id');
+        res.body[0].should.have.property('date_generated');
+        res.body[0].should.have.property('freshest_input_date');
+        res.body[0].should.have.property('files_in');
+        res.body[0].should.have.property('report_path');
+        res.body[0].should.have.property('notification_targets');
+        res.body[0].should.have.property('notification_message');
+        res.body[0].should.have.property('notification_done');
+        res.body[0].should.have.property('parameters');
+        done();
+      });
+  });
+
+  it('should not find report records by given name, version and files_in', function(done) {
+    chai.request(server)
+      .post('/reportdb/record_files?name=jsonReport&version=1.1')
+      .send ({
+       "files_in": {
+          "file_path":["p1", "p2", "p3", "p5"]
+        }
+      })
+      .end(function(err, res){
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body.should.deep.equal([]);
+        done();
+      });
+  });
+
+  it('should get report records by given name, version and parameters', function(done) {
+    chai.request(server)
+      .post('/reportdb/record_parameters?name=jsonReport&version=1.0')
+      .send ({
+        "parameters": {
+          "runName":"170623_D00331_0244_BCB1LMANXX", 
+          "instrument":"HiSeq"
+        }
+      })
+      .end(function(err, res){
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body[0].should.have.property('report_record_id');
+        res.body[0].should.have.property('report_id');
+        res.body[0].should.have.property('date_generated');
+        res.body[0].should.have.property('freshest_input_date');
+        res.body[0].should.have.property('files_in');
+        res.body[0].should.have.property('report_path');
+        res.body[0].should.have.property('notification_targets');
+        res.body[0].should.have.property('notification_message');
+        res.body[0].should.have.property('notification_done');
+        res.body[0].should.have.property('parameters');
+        done();
+      });
+  });
+
+  it('should not find get report records by given name, version and parameters', function(done) {
+    chai.request(server)
+      .post('/reportdb/record_parameters?name=jsonReport&version=1.0')
+      .send ({
+        "parameters": {
+          "runName":"170623_D00331_0244_BCB1LMANXX", 
+          "instrument":"MiSeq"
+        }
+      })
+      .end(function(err, res){
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body.should.deep.equal([]);
+        done();
+      });
+  });
+});
+
