36e7896e1f08a07f801acf8cc1791c6ffcb37159
Heather Armstrong <heather.armstrong@oicr.on.ca>
2018-03-01 16:31:23 -0500

    Ignore favicon for now

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 require('dotenv').config();
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+//var favicon = require('serve-favicon'); // re-add when we get a favicon
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
