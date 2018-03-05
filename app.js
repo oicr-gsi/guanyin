e7437eb548578f96eb9cb76330404f685ccc08c1
Heather Armstrong <heather.armstrong@oicr.on.ca>
2018-03-05 16:26:14 -0500

    log request URL and method

--- app.js
+++ app.js
@@ -47,12 +47,17 @@ app.get('/swagger.json', function(req, res) {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.use((req, res, next) => {
+  // log the request method and URL
+  logger.info({ method: req.method, url: req.originalUrl });
+  next();
+});
 
 app.use('/', index);
 app.get('/metrics', async (req, res) => {
