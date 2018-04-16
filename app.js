bff7948bcca5f88eec92684a1cf2b1c566202e4c
Andre Masella <andre.masella@oicr.on.ca>
2018-04-16 16:20:46 -0400

    Increase allowed body size

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.set('view engine', 'jade');
 
 app.use(morgan('common'));
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
