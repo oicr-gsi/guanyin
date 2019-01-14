f289c295af983bc048ca906d92a963e77874a18b
Heather Armstrong <callunity@users.noreply.github.com>
2019-01-14 13:54:34 -0500

    Use Docker for Flyway (#24)
    * Update to use Flyway Docker image
    
    * Use Docker Flyway image for testing too

---
+++ test/setup_test_db.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# since we need to mount all the migrations in a single directory, the "regular" migrations need to be
+# copied into the test folder, then deleted after.
+cp sql/V*.sql test/sql/
+
+docker run --rm -v $(pwd)/test:/flyway/conf -v $(pwd)/sql:/flyway/sql --network=host boxfuse/flyway clean && \
+docker run --rm -v $(pwd)/test:/flyway/conf -v $(pwd)/test/sql:/flyway/sql --network=host boxfuse/flyway migrate
+
+find test/sql/ -type f ! -name 'V9*.sql' -delete
