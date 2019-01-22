e2ce1f87320bcbb371394a9bf801075881abd9a5
Heather Armstrong <heather.armstrong@oicr.on.ca>
2019-01-22 14:35:37 -0500

    future-proof db migrations

--- test/setup_test_db.sh
+++ test/setup_test_db.sh
@@ -7,4 +7,4 @@ cp sql/V*.sql test/sql/
 docker run --rm -v $(pwd)/test:/flyway/conf -v $(pwd)/sql:/flyway/sql --network=host boxfuse/flyway clean && \
 docker run --rm -v $(pwd)/test:/flyway/conf -v $(pwd)/test/sql:/flyway/sql --network=host boxfuse/flyway migrate
 
-find test/sql/ -type f ! -name 'V9*.sql' -delete
+find test/sql/ -type f ! -name 'V999*.sql' -delete
