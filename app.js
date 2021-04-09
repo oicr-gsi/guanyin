2f6cbd40b1b382f44ec106b7d79a00630aa0c325
Heather Armstrong <callunity@users.noreply.github.com>
2021-04-09 15:04:37 -0400

    Merge fixes that were present in the production machine's repository, as they are clearly relevant (#41)

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.set('view engine', 'jade');
 
 app.use(morgan('common'));
 
-app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.json({ limit: '500mb' }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
