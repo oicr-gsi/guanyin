7ae3282c051078d6c409f2b05338bed343449c5d
grapigeau <gpigeau@oicr.on.ca>
2023-02-07 09:39:10 -0500

    update deps and modify code as needed

--- utils/logger.js
+++ utils/logger.js
@@ -1,35 +1,39 @@
+
 'use strict';
 
 const winston = require('winston');
 const logLocation = process.env.LOG_LOCATION || 'logs';
 
-const logger = new winston.Logger({
-  transports: [
-    new winston.transports.File({
-      name: 'combined-log',
-      filename: `${logLocation}/combined.log`,
-      level: 'info',
-      colorize: true,
-      timestamp: 'tsFormat'
-    }),
-    new winston.transports.File({
-      name: 'error-log',
-      filename: `${logLocation}/error.log`,
-      level: 'error',
-      handleException: true,
-      humanReadableUnhandledException: true,
-      colorize: true,
-      timestamp: 'tsFormat'
-    })
-  ]
-});
+const logger = winston.createLogger();
+if (process.env.NODE_ENV !== 'test'){ 
+  logger.add({
+    transports: [
+      new winston.transports.File({
+        name: 'combined-log',
+        filename: `${logLocation}/combined.log`,
+        level: 'info',
+        colorize: true,
+        timestamp: 'tsFormat'
+      }),
+      new winston.transports.File({
+        name: 'error-log',
+        filename: `${logLocation}/error.log`,
+        level: 'error',
+        handleException: true,
+        humanReadableUnhandledException: true,
+        colorize: true,
+        timestamp: 'tsFormat'
+      })
+    ]
+  });
+}
 
 if (process.env.NODE_ENV !== 'production' || process.env.LOG_LEVEL != 'prod') {
-  logger.add(winston.transports.Console, {
+  logger.add(new winston.transports.Console({
     level: 'debug',
     colorize: true,
     timestamp: 'tsFormat'
-  });
+  }));
 }
 
 module.exports = logger;
